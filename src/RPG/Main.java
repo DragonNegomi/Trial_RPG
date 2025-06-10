c04d5c6dbcf334e9df8701a972522ceded8534ce
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-11 00:13:59 +0100

    Added RoomDetailsPanel and commented out ExploreOptionsPanel

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -2,6 +2,7 @@ package RPG;
 
 import RPG.gui.*;
 import RPG.player.Player;
+import RPG.room.Room;
 
 import javax.swing.*;
 
@@ -14,15 +15,21 @@ public class Main {
         JFrame frame = new JFrame("RPG");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // PlayerStatsPanel
         Player player = new Player("Bob", "Human", "Monk", 5, 3, 10, 56, 8, 7, 9, 10, 6, 14, null, null, null, null, null);
         PlayerStatsPanel playerStatsPanel = new PlayerStatsPanel(player);
+        // RoomDetailsPanel
+        Room room = new Room("K", "A large kitchen----------------");
+        RoomDetailsPanel roomDetailsPanel = new RoomDetailsPanel(room);
+        // ExploreOptionsPanel
         ExploreOptionsPanel exploreOptionsPanel = new ExploreOptionsPanel();
 
         JPanel containerPanel = new JPanel();
         containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
 
         containerPanel.add(playerStatsPanel);
-        containerPanel.add(exploreOptionsPanel);
+        containerPanel.add(roomDetailsPanel);
+        //containerPanel.add(exploreOptionsPanel);
 
         frame.add(containerPanel);
 
