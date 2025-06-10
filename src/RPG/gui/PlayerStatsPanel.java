4618599c50510347ccb36ac1ad7bb12da648c608
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-11 00:12:37 +0100

    Changed layout of panel

--- src/RPG/gui/PlayerStatsPanel.java
+++ src/RPG/gui/PlayerStatsPanel.java
@@ -14,31 +14,38 @@ public class PlayerStatsPanel extends JPanel{
      * @param player The Player object containing the player information
      */
     public PlayerStatsPanel(Player player){
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
         //setBackground(Color.BLUE);
         // Create 2 panels so attributes display in lines
         JPanel playerInformation = new JPanel();
         JPanel playerAttributes = new JPanel();
-        playerInformation.setBackground(Color.LIGHT_GRAY);
-        playerAttributes.setBackground(Color.LIGHT_GRAY);
-        setBackground(Color.GRAY);
+
+        // Set layout for panels
+        playerInformation.setLayout(new BoxLayout(playerInformation, BoxLayout.X_AXIS));
+        playerAttributes.setLayout(new BoxLayout(playerAttributes, BoxLayout.X_AXIS));
+
+        //playerInformation.setBackground(Color.LIGHT_GRAY);
+        //playerAttributes.setBackground(Color.LIGHT_GRAY);
+        //setBackground(Color.GRAY);
 
         // Creates labels for each player attribute
         JLabel playerName = new JLabel("Name: " + player.getPlayerName());
-        JLabel playerRace = new JLabel("   Race: " + player.getPlayerRace());
-        JLabel playerClass = new JLabel("   Class: " + player.getPlayerClass());
-        JLabel playerLevel = new JLabel("   Level: " + player.getPlayerLevel());
-        JLabel playerExperience = new JLabel("   Experience: " + player.getPlayerExperience());
-        JLabel playerHealth = new JLabel("   Health: " + player.getPlayerHealth());
-        JLabel playerMoney = new JLabel("   Money: " + player.getPlayerMoney());
+        JLabel playerRace = new JLabel("    Race: " + player.getPlayerRace());
+        JLabel playerClass = new JLabel("    Class: " + player.getPlayerClass());
+        JLabel playerLevel = new JLabel("    Level: " + player.getPlayerLevel());
+        JLabel playerExperience = new JLabel("    Experience: " + player.getPlayerExperience());
+        JLabel playerHealth = new JLabel("    Health: " + player.getPlayerHealth());
+        JLabel playerMoney = new JLabel("    Money: " + player.getPlayerMoney());
         JLabel playerStrength = new JLabel("Strength: " + player.getPlayerStrength());
-        JLabel playerDexterity = new JLabel("   Dexterity: " + player.getPlayerDexterity());
-        JLabel playerConstitution = new JLabel("   Constitution: " + player.getPlayerConstitution());
-        JLabel playerIntelligence = new JLabel("   Intelligence: " + player.getPlayerIntelligence());
-        JLabel playerWisdom = new JLabel("   Wisdom: " + player.getPlayerWisdom());
-        JLabel playerCharisma = new JLabel("   Charisma: " + player.getPlayerCharisma());
-        playerCharisma.setForeground(Color.ORANGE);
-        playerCharisma.setOpaque(true);
-        playerCharisma.setBackground(Color.BLUE);
+        JLabel playerDexterity = new JLabel("    Dexterity: " + player.getPlayerDexterity());
+        JLabel playerConstitution = new JLabel("    Constitution: " + player.getPlayerConstitution());
+        JLabel playerIntelligence = new JLabel("    Intelligence: " + player.getPlayerIntelligence());
+        JLabel playerWisdom = new JLabel("    Wisdom: " + player.getPlayerWisdom());
+        JLabel playerCharisma = new JLabel("    Charisma: " + player.getPlayerCharisma());
+        //playerCharisma.setForeground(Color.ORANGE);
+        //playerCharisma.setOpaque(true);
+        //playerCharisma.setBackground(Color.BLUE);
 
         // Adds labels to playerInformation panel
         playerInformation.add(playerName);
@@ -58,11 +65,15 @@ public class PlayerStatsPanel extends JPanel{
         playerAttributes.add(playerCharisma);
 
         // Changes sizes to fit look
-        playerInformation.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0));
-        playerAttributes.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        //playerInformation.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        //playerAttributes.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0));
         // Adds separate panels to main panel
+        add(Box.createRigidArea(new Dimension(0, 10)));
         add(playerInformation);
         add(playerAttributes);
-        setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
+        add(Box.createRigidArea(new Dimension(0, 30)));
+        //setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
+        //setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
     }
 }
