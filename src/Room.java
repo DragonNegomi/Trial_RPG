d2d37d4edb063c4aec2d18c3ae57bd06a2cdddec
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-10 17:05:19 +0100

    Create Room.java
    Room object currently just has name

---
+++ src/Room.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * This class is used to represent a room in the game
+ *
+ * Each room can contain items and characters
+ */
+public class Room {
+    private String name;
+    private ArrayList<Room> rooms;
+
+    /**
+     * Creates a new room
+     * @param name Name of the room e.g. Kitchen
+     */
+    public Room(String name) {
+        setName(name);
+    }
+
+    /**
+     * Returns the name of the room
+     * @return Name of the room
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the room
+     * @param name Name of the room
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
