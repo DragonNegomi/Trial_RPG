68a688148de7640594264d24d064c7bdfd0d2537
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-10 17:09:56 +0100

    Create Character.java
    Character object currently only has name

---
+++ src/Character.java
@@ -0,0 +1,32 @@
+/**
+ * This class represents a character in the game
+ */
+public class Character {
+    private String name;
+
+
+    /**
+     * Constructor for character object
+     * Sets name of character
+     * @param name Name of character
+     */
+    public Character(String name) {
+        setName(name);
+    }
+
+    /**
+     * Returns name of character
+     * @return Name of character
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name of character
+     * @param name Name of character
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
