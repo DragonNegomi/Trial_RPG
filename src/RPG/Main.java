b13257df3d975e5caede3910c8d2eee857df6b63
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-12 17:12:12 +0100

    Added inventory panel to display

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -15,26 +15,42 @@ public class Main {
         JFrame frame = new JFrame("RPG");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // PlayerStatsPanel
+        // Create player
         Player player = new Player("Bob", "Human", "Monk", 5, 3, 10, 56, 8, 9, 10, 11, 12, 13, null, null, null, null, null);
         player.setPlayerExtraStrength(2);
         player.setPlayerExtraWisdom(-1);
-        PlayerStatsPanel playerStatsPanel = new PlayerStatsPanel(player);
-        // RoomDetailsPanel
+        // Create room
         Room room = new Room("K", "A large kitchen----------------");
+
+        // Create PlayerStatsPanel (requires player)
+        PlayerStatsPanel playerStatsPanel = new PlayerStatsPanel(player);
+        // Create RoomDetailsPanel (requires room)
         RoomDetailsPanel roomDetailsPanel = new RoomDetailsPanel(room);
-        // ExploreOptionsPanel
+        // Create ExploreOptionsPanel
         ExploreOptionsPanel exploreOptionsPanel = new ExploreOptionsPanel();
+        // Create PlayerInventoryPanel (requires player)
+        PlayerInventoryPanel playerInventoryPanel = new PlayerInventoryPanel(player);
 
-        JPanel containerPanel = new JPanel();
-        containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+        // Create panel to contain player and room info
+        JPanel innerContainerPanel = new JPanel();
+        innerContainerPanel.setLayout(new BoxLayout(innerContainerPanel, BoxLayout.Y_AXIS));
+        // Create panel to contain all other panels
+        JPanel outerContainerPanel = new JPanel();
+        outerContainerPanel.setLayout(new BoxLayout(outerContainerPanel, BoxLayout.X_AXIS));
 
-        containerPanel.add(playerStatsPanel);
-        containerPanel.add(roomDetailsPanel);
-        //containerPanel.add(exploreOptionsPanel);
+        // Adds player and room  info to inner container panel
+        innerContainerPanel.add(playerStatsPanel);
+        innerContainerPanel.add(roomDetailsPanel);
+        //innerContainerPanel.add(exploreOptionsPanel);
 
-        frame.add(containerPanel);
+        // Adds inventory and inner container to outer container
+        outerContainerPanel.add(playerInventoryPanel);
+        outerContainerPanel.add(innerContainerPanel);
 
+        // Adds outer container to frame
+        frame.add(outerContainerPanel);
+
+        // Packs frame, sets size and makes visible
         frame.pack();
         frame.setSize(800, 600);
         frame.setVisible(true);
