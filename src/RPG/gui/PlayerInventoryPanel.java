1a018fe8e314db8628f43ba04fd1a282ee6e59ab
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-12 17:39:05 +0100

    Added blank and titled border

--- src/RPG/gui/PlayerInventoryPanel.java
+++ src/RPG/gui/PlayerInventoryPanel.java
@@ -4,12 +4,20 @@ import RPG.item.Item;
 import RPG.player.Player;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class PlayerInventoryPanel extends JPanel{
 
     public PlayerInventoryPanel(Player player){
         setBackground(Color.GRAY);
+
+        // Creates blank border
+        Border blankBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        // Creates titled border
+        Border titleBorder = BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), "Player Inventory");
+         // Sets blank border around titled border
+        setBorder(BorderFactory.createCompoundBorder(blankBorder, titleBorder));
 
         // Creates and adds title label
         JLabel inventoryLabel = new JLabel("Inventory");
