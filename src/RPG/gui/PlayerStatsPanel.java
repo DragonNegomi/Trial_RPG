e4989f2b1af00f537a9a96856563031d25c050a8
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-12 18:11:54 +0100

    Added proper spacing within panel instead of spaces in labels

--- src/RPG/gui/PlayerStatsPanel.java
+++ src/RPG/gui/PlayerStatsPanel.java
@@ -34,12 +34,12 @@ public class PlayerStatsPanel extends JPanel{
 
         // Creates labels for each player base info
         JLabel playerName = new JLabel("Name: " + player.getPlayerName());
-        JLabel playerRace = new JLabel("    Race: " + player.getPlayerRace());
-        JLabel playerClass = new JLabel("    Class: " + player.getPlayerClass());
-        JLabel playerLevel = new JLabel("    Level: " + player.getPlayerLevel());
-        JLabel playerExperience = new JLabel("    Experience: " + player.getPlayerExperience());
-        JLabel playerHealth = new JLabel("    Health: " + player.getPlayerHealth());
-        JLabel playerMoney = new JLabel("    Money: " + player.getPlayerMoney());
+        JLabel playerRace = new JLabel("Race: " + player.getPlayerRace());
+        JLabel playerClass = new JLabel("Class: " + player.getPlayerClass());
+        JLabel playerLevel = new JLabel("Level: " + player.getPlayerLevel());
+        JLabel playerExperience = new JLabel("Experience: " + player.getPlayerExperience());
+        JLabel playerHealth = new JLabel("Health: " + player.getPlayerHealth());
+        JLabel playerMoney = new JLabel("Money: " + player.getPlayerMoney());
         // Creates strength label including extra if any
         JLabel playerStrength;
         if (player.getPlayerExtraStrength()==0) {
@@ -49,52 +49,64 @@ public class PlayerStatsPanel extends JPanel{
         // Creates dexterity label including extra if any
         JLabel playerDexterity;
         if (player.getPlayerExtraDexterity()==0) {
-            playerDexterity = new JLabel("    Dexterity: " + player.getPlayerActualDexterity());
+            playerDexterity = new JLabel("Dexterity: " + player.getPlayerActualDexterity());
         }
-        else {playerDexterity = new JLabel ("    Dexterity: " + player.getPlayerActualDexterity() + " (" + player.getPlayerExtraDexterityString() + ")");}
+        else {playerDexterity = new JLabel ("Dexterity: " + player.getPlayerActualDexterity() + " (" + player.getPlayerExtraDexterityString() + ")");}
         // Creates constitution label including extra if any
         JLabel playerConstitution;
         if (player.getPlayerExtraConstitution()==0) {
-            playerConstitution = new JLabel("    Constitution: " + player.getPlayerActualConstitution());
+            playerConstitution = new JLabel("Constitution: " + player.getPlayerActualConstitution());
         }
-        else {playerConstitution = new JLabel("    Constitution: " + player.getPlayerActualConstitution() + " (" + player.getPlayerExtraConstitutionString() + ")");}
+        else {playerConstitution = new JLabel("Constitution: " + player.getPlayerActualConstitution() + " (" + player.getPlayerExtraConstitutionString() + ")");}
         // Creates intelligence label including extra if any
         JLabel playerIntelligence;
         if (player.getPlayerExtraIntelligence()==0) {
-            playerIntelligence = new JLabel("    Intelligence: " + player.getPlayerActualIntelligence());
+            playerIntelligence = new JLabel("Intelligence: " + player.getPlayerActualIntelligence());
         }
-        else {playerIntelligence = new JLabel("    Intelligence: " + player.getPlayerActualIntelligence() + " (" + player.getPlayerExtraIntelligenceString() + ")");}
+        else {playerIntelligence = new JLabel("Intelligence: " + player.getPlayerActualIntelligence() + " (" + player.getPlayerExtraIntelligenceString() + ")");}
         // Creates wisdom label including extra if any
         JLabel playerWisdom;
         if (player.getPlayerExtraWisdom()==0) {
-            playerWisdom = new JLabel("    Wisdom: " + player.getPlayerActualWisdom());
+            playerWisdom = new JLabel("Wisdom: " + player.getPlayerActualWisdom());
         }
-        else {playerWisdom = new JLabel("    Wisdom: " + player.getPlayerActualWisdom() + " (" + player.getPlayerExtraWisdomString() + ")");}
+        else {playerWisdom = new JLabel("Wisdom: " + player.getPlayerActualWisdom() + " (" + player.getPlayerExtraWisdomString() + ")");}
         // Creates charisma label including extra if any
         JLabel playerCharisma;
         if (player.getPlayerExtraCharisma()==0) {
-            playerCharisma = new JLabel("    Charisma: " + player.getPlayerActualCharisma());
+            playerCharisma = new JLabel("Charisma: " + player.getPlayerActualCharisma());
         }
-        else {playerCharisma = new JLabel("    Charisma: " + player.getPlayerActualCharisma() + " (" + player.getPlayerExtraCharismaString() + ")");}
+        else {playerCharisma = new JLabel("Charisma: " + player.getPlayerActualCharisma() + " (" + player.getPlayerExtraCharismaString() + ")");}
         //playerCharisma.setForeground(Color.ORANGE);
         //playerCharisma.setOpaque(true);
         //playerCharisma.setBackground(Color.BLUE);
 
+        // Creates blank component
         // Adds labels to playerInformation panel
         playerInformation.add(playerName);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerRace);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerClass);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerLevel);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerExperience);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerHealth);
+        playerInformation.add(Box.createRigidArea(new Dimension(15, 0)));
         playerInformation.add(playerMoney);
 
         // Adds labels to playerAttributes panel
         playerAttributes.add(playerStrength);
+        playerAttributes.add(Box.createRigidArea(new Dimension(15, 0)));
         playerAttributes.add(playerDexterity);
+        playerAttributes.add(Box.createRigidArea(new Dimension(15, 0)));
         playerAttributes.add(playerConstitution);
+        playerAttributes.add(Box.createRigidArea(new Dimension(15, 0)));
         playerAttributes.add(playerIntelligence);
+        playerAttributes.add(Box.createRigidArea(new Dimension(15, 0)));
         playerAttributes.add(playerWisdom);
+        playerAttributes.add(Box.createRigidArea(new Dimension(15, 0)));
         playerAttributes.add(playerCharisma);
 
         // Adds separate panels to main panel
