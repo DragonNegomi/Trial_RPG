cb0a183b158286da8d3c3733793f8a2f08ffdaf0
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-12 17:11:42 +0100

    Added basic panel details

--- src/RPG/gui/PlayerInventoryPanel.java
+++ src/RPG/gui/PlayerInventoryPanel.java
@@ -1,4 +1,24 @@
 package RPG.gui;
 
-public class PlayerInventoryPanel {
+import RPG.item.Item;
+import RPG.player.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerInventoryPanel extends JPanel{
+
+    public PlayerInventoryPanel(Player player){
+        setBackground(Color.GRAY);
+
+        // Creates and adds title label
+        JLabel inventoryLabel = new JLabel("Inventory");
+        add(inventoryLabel);
+
+        // Creates and adds labels of all item names
+        for (Item item : player.getInventory()){
+            JLabel itemLabel = new JLabel(item.getName());
+            add(itemLabel);
+        }
+    }
 }
