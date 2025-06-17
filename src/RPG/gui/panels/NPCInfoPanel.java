08e75b225f0bdf948f2968aa2b194548fded975b
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-17 03:30:18 +0100

    Added basic info to the panel

--- src/RPG/gui/panels/NPCInfoPanel.java
+++ src/RPG/gui/panels/NPCInfoPanel.java
@@ -1,4 +1,15 @@
 package RPG.gui.panels;
 
+import RPG.character.NonPlayerCharacter;
+
+import javax.swing.*;
+
 public class NPCInfoPanel extends JPanel {
+
+    public NPCInfoPanel(NonPlayerCharacter NPC) {
+        setBorder(BorderFactory.createTitledBorder(NPC.getName()));
+
+        JLabel nameLabel = new JLabel(NPC.getName());
+        add(nameLabel);
+    }
 }
