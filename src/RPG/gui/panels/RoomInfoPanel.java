8ae7fa0e8754157bf2672240170625c45e17afb9
DragonNegomi <I.J.Heap2@newcastle.ac.uk>
2025-06-18 14:52:03 +0100

    Removed commented out code and unnecessary import

--- src/RPG/gui/panels/RoomInfoPanel.java
+++ src/RPG/gui/panels/RoomInfoPanel.java
@@ -6,7 +6,6 @@ import RPG.room.Room;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Map;
 
 public class RoomInfoPanel extends JPanel{
 
@@ -33,51 +32,6 @@ public class RoomInfoPanel extends JPanel{
                 add(roomInfoButton);
             }
         }
-
-        /*for (Map.Entry<String, String> entry : room.getInfo().entrySet()) {
-            if (entry.getKey() == "String") {
-                roomTextInfoLabel = new JLabel(entry.getValue());
-                roomTextInfoLabel.setOpaque(true);
-                roomTextInfoLabel.setBackground(Color.PINK);
-                add(roomTextInfoLabel);
-            }
-            else if (entry.getKey() == "NPC") {
-                roomInfoButton = new NPCButton(entry.getValue());
-                add(roomInfoButton);
-            }
-            else if (entry.getKey() == "RoomObject") {
-                roomInfoButton = new JButton(entry.getValue());
-                roomInfoButton.setBorderPainted(false);
-                roomInfoButton.setFocusPainted(false);
-                roomInfoButton.setContentAreaFilled(false);
-                roomInfoButton.setOpaque(true);
-                roomInfoButton.setBackground(Color.PINK);
-                roomInfoButton.setForeground(Color.BLUE);
-                roomInfoButton.setMargin(new Insets(-3, 0, -3, 0));
-                add(roomInfoButton);
-            }
-        }
-
-
-        for (int i = 0; i < room.getInfo().size(); i++) {
-            if (i % 2 == 0) {
-                roomTextInfoLabel = new JLabel(room.getInfo().get(i));
-                roomTextInfoLabel.setOpaque(true);
-                roomTextInfoLabel.setBackground(Color.PINK);
-                add(roomTextInfoLabel);
-            }
-            else {
-                roomInfoButton = new JButton(room.getInfo().get(i));
-                roomInfoButton.setBorderPainted(false);
-                roomInfoButton.setFocusPainted(false);
-                roomInfoButton.setContentAreaFilled(false);
-                roomInfoButton.setOpaque(true);
-                roomInfoButton.setBackground(Color.PINK);
-                roomInfoButton.setForeground(Color.BLUE);
-                roomInfoButton.setMargin(new Insets(-3, 0, -3, 0));
-                add(roomInfoButton);
-            }
-        }
-         */
+        
     }
 }
